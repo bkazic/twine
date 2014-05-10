6eb67627011a5abf8bd2e8c4c401db2e0abc152d
bkazic <blaz.kazic@ijs.si>
2014-05-10 15:05:56 +0200

    First commit. Initialize store from twine.def Add one test record Starts server Has query function and add function

---
+++ src/twine.js
@@ -0,0 +1,34 @@
+
+// open store from def file
+var twineStore = qm.store("twineMeasurements");
+
+// testing querying
+var recs = qm.search({"$from":"twineMeasurements"});
+console.say(JSON.stringify(recs));
+
+//add test record
+var rec = {"Temperature":24,"Orientation":"Top","Vibration":1345};
+twineStore.add(rec);
+
+// check if the record was added
+console.say(JSON.stringify(twineStore.recs));
+
+
+// ONLINE SERVICES
+// http://localhost:8080/twine/query?data={"$from":"twineMeasurements"}
+// Query
+http.onGet("query", function (req, resp) {
+  jsonData = JSON.parse(req.args.data);
+  console.log("" + JSON.stringify(jsonData));
+  var recs = qm.search(jsonData);
+  return jsonp(req, resp, recs);
+});
+
+// http://localhost:8080/twine/add?data={"Temperature":24,"Orientation":"Top","Vibration":1345}
+// Add measurement from twineStore
+http.onGet("add", function (req, resp) {
+  rec = JSON.parse(req.args.data);
+  twineStore.add(rec);
+  console.log("New measurement added: " + JSON.stringify(rec));
+  return jsonp(req, resp, "OK");
+});
